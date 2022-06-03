0d40ee90e6275c6ca088ab39d3deee9a1d769e51
Murugan-Karthick <106802874+Murugan-Karthick@users.noreply.github.com>
2022-06-03 17:15:19 +0530

    Update Q-learning.py

--- Q-Learning/Q-learning.py
+++ Q-Learning/Q-learning.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-## Reward matrix
+# Reward matrix
 R = np.matrix([[-1,-1,-1,-1,0,-1],
                [-1,-1,-1,0,-1,100],
                [-1,-1,-1,0,-1,-1],
@@ -10,20 +9,17 @@ R = np.matrix([[-1,-1,-1,-1,0,-1],
                [-1,0,-1,-1,0,100]])
 
 
-## Q-matrix
-
+# Q-matrix
 Q = np.matrix(np.zeros([6,6]))
 
-## Gama parameter
+# Gaama parameter
 gamma = 0.8
 
-## Initial state (usually chosen at random)
-
+# Initial state (usually chosen at random)
 initial_state  = 1
 
 
-## Get all the available actions in a particular state
-
+# Get all the available actions in a particular state
 def available_action(state):
     current_state_row = R[state,]
     av_act  = np.where(current_state_row>=0)[1]
@@ -31,20 +27,18 @@ def available_action(state):
 
 available_act = available_action(initial_state)
 
-### function return random actions among all the available actions within the range
-
+# function return random actions among all the available actions within the range
 def sample_next_action(available_action_range):
     next_action = int(np.random.choice(available_action_range,1))
     return next_action
 
 
-#sample next action to be performed
-
+# sample next action to be performed
 action=sample_next_action(available_act)
 
 
 
-## Function updates the Q matrix according to the path selected and the Q
+# Function updates the Q matrix according to the path selected and the Q
 
 def update(current_state, action, gamma):
     max_index = np.where(Q[action,]==np.max(Q[action,]))[1]
@@ -54,7 +48,7 @@ def update(current_state, action, gamma):
         max_index = int(max_index)
     max_value = Q[action,max_index]
     
-    #Q learning
+    # Q learning
     Q[current_state,action] = R[current_state, action]+gamma*max_value
 
 
@@ -62,10 +56,10 @@ update(initial_state,action,gamma)
 
 
 
-## Training ---------------------------------------------------------------------------------
+# ****************Training or Learning Q Matrix******************
 
 
-## Train over 10 000 iterations  (re iterate the process above)
+# Train over 10 000 iterations  (re iterate the process above)
 for i in range(0,10000):
     current_state = np.random.randint(0,int(Q.shape[0]))
     available_act = available_action(current_state)
@@ -77,9 +71,8 @@ print(Q/np.max(Q)*100)
 
 
 
-## Testing -----------------------------------------------------------------------------------
-
-## Any number [0-5] can be given to test the traversal to the goal (goal would be 5)
+# ****************Testing**************** 
+# Any number [0-5] can be given to test the traversal to the goal (goal would be 5)
 current_state = 5
 steps = [current_state]
 
